95f3508ad84ec317aa11b4b8c0c2f357a98482f3
Sakna Perera <37286973+sakna1@users.noreply.github.com>
2025-10-14 22:30:12 +0530

    Before deploy

--- config.py
+++ config.py
@@ -2,10 +2,12 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
+
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv(
-        'DATABASE_URL', 'postgresql://postgres:1234@localhost/mediflow')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://postgres:1234@localhost/mediflow')
+    if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv('SECRET_KEY', 'devsecret')
-
     GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
